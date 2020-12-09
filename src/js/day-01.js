e2c7f94a3564949c9f09682374aa5124f5810e58
Eric Butler <ericbutler000@gmail.com>
2020-12-09 11:19:36 -0800

    Added a JS solution to Day 1

---
+++ src/js/day-01.js
@@ -0,0 +1,30 @@
+function crunchNums1(nums) {
+  for (num1 of nums) {
+    for (num2 of nums) {
+      if (num1 + num2 == 2020) {
+        return [num1, num2];
+      }
+    }
+  }
+}
+
+function crunchNums2(num) {
+  for (num1 of nums) {
+    for (num2 of nums) {
+      for (num3 of nums) {
+        if (num1 + num2 + num3 == 2020) {
+          return [num1, num2, num3];
+        }
+      }
+    }
+  }
+}
+
+const contents = require('fs').readFileSync('day-01-input.txt', { encoding: 'utf8' });
+const nums = contents.split('\n').map(x => parseInt(x));
+
+const answer1 = crunchNums1(nums);
+console.log(`Part 1 - ${answer1[0]} & ${answer1[1]} = ${answer1[0] * answer1[1]}`);
+
+const answer2 = crunchNums2(nums);
+console.log(`Part 2 - ${answer2[0]} & ${answer2[1]} & ${answer2[2]} = ${answer2[0] * answer2[1] * answer2[2]}`);
